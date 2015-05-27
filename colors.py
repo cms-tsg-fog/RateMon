0719501607df79e2835361b6600270e56e3f28c5
zucchett <a.zucchetta@cern.ch>
2015-05-27 16:27:02 +0200

    Switched to bold colours

--- colors.py
+++ colors.py
@@ -1,9 +1,9 @@
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    OKGREEN = '\033[1;92m'
+    WARNING = '\033[1;93m'
+    FAIL = '\033[1;91m'
     ENDC = '\033[0m'
     
     def disable(self):
