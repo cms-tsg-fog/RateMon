00deeafffd3a799763d32dab1ab3510e1812e382
Alberto <a.zucchetta@cern.ch>
2015-09-19 18:55:23 +0200

    Disabled Stream display by default

--- ShiftMonitorNCR.py
+++ ShiftMonitorNCR.py
@@ -106,7 +106,7 @@ class ShiftMonitor:
         self.quiet = False              # Prints fewer messages in this mode
         self.noColors = False           # Special formatting for if we want to dump the table to a file
         self.sendMailAlerts = False     # Whether we should send alert mails
-        self.showStreams = True         # Whether we should print stream information
+        self.showStreams = False        # Whether we should print stream information
         self.showPDs = False            # Whether we should print pd information
         self.totalStreams = 0           # The total number of streams
         self.maxStreamRate = 1000000    # The maximum rate we allow a "good" stream to have
