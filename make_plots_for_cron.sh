343dc6a2523ad9d7b61b56c6521f360abbe2260c
geoff-smith <geoff127@gmail.com>
2016-09-28 19:06:47 +0200

    clean up some commented lines

--- make_plots_for_cron.sh
+++ make_plots_for_cron.sh
@@ -3,32 +3,11 @@
 cd /afs/cern.ch/work/g/gesmith/runCert/RateMon3/RateMon/
 outputDirBase=/afs/cern.ch/user/g/gesmith/www/HLT/
 
-#theFill=$(python test_dump_fills_runs.py)
 theFillsAndRuns=$(python test_dump_fills_runs.py)
-
-#echo $theRuns
-echo " "
-
-#IFS=' ' read -ra fillAndRuns <<< "$theRuns"
-#echo $fillAndRuns
-#
-#for i in "${fillAndRuns[@]}"; do
-#    echo $i
-#done
-
-
-#fillAndRuns=$(echo $theRuns | tr " " "\n")
 
 theRuns="${theFillsAndRuns[@]:5}"
 theFill="${theFillsAndRuns[@]:0:4}"
 
-
-#for number in $fillAndRuns
-#do
-#    echo $number
-#done
-
-
-mkdir outputDirBase$theFill
+#mkdir outputDirBase$theFill
 #python plotTriggerRates.py --triggerList=monitorlist_COLLISIONS.list --fitFile=Fits/2016/FOG.pkl --saveDirectory=$outputDirBase$theFill --useFills $theFill
 python plotTriggerRates.py --triggerList=monitorlist_COLLISIONS.list --fitFile=Fits/2016/FOG.pkl --saveDirectory=$outputDirBase$theFill $theRuns
