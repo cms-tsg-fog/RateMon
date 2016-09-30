44417e67412c2f05017ea85f7fa78a214833ff7d
geoff-smith <geoff127@gmail.com>
2016-09-30 18:15:11 +0200

    cron-related updates

--- make_plots_for_cron.sh
+++ make_plots_for_cron.sh
@@ -11,3 +11,15 @@ theFill="${theFillsAndRuns[@]:0:4}"
 #mkdir outputDirBase$theFill
 #python plotTriggerRates.py --triggerList=monitorlist_COLLISIONS.list --fitFile=Fits/2016/FOG.pkl --saveDirectory=$outputDirBase$theFill --useFills $theFill
 python plotTriggerRates.py --triggerList=monitorlist_COLLISIONS.list --fitFile=Fits/2016/FOG.pkl --saveDirectory=$outputDirBase$theFill $theRuns
+
+while read -r line
+do
+    fitCommand="$line"
+done < "Fits/2016/command_line.txt"
+
+match='<html>'
+insertFirst='Runs used to produce fits:<br>'
+insertSecond=$(echo $fitCommand | sed 's/python plotTriggerRates.py --createFit --nonLinear --triggerList=monitorlist_COLLISIONS.list//')
+file=$outputDirBase$theFill/index.html
+
+sed -i "s/$match/$match\n$insertFirst\n$insertSecond/" $file
