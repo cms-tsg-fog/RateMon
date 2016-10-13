eefcc23d8c2166c1d69725a906e64f76742e503f
geoff-smith <geoff127@gmail.com>
2016-10-13 17:37:33 +0200

    cron updates (adding more triggers)

--- make_plots_for_cron.sh
+++ make_plots_for_cron.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
+# crontab line for this is:
+# 59 * * * * lxplus.cern.ch /afs/cern.ch/work/g/gesmith/runCert/RateMon3/RateMon/make_plots_for_cron.sh > /dev/null
 
-cd /afs/cern.ch/work/g/gesmith/runCert/RateMon3/RateMon/
+thisDir=/afs/cern.ch/work/g/gesmith/runCert/RateMon3/RateMon/
 outputDirBase=/afs/cern.ch/user/g/gesmith/www/HLT/RateVsPU/
+
+cd $thisDir
 
 theFillsAndRuns=$(python test_dump_fills_runs.py)
 
 theRuns="${theFillsAndRuns[@]:5}"
 theFill="${theFillsAndRuns[@]:0:4}"
 
-#mkdir outputDirBase$theFill
-#python plotTriggerRates.py --triggerList=monitorlist_COLLISIONS.list --fitFile=Fits/2016/FOG.pkl --saveDirectory=$outputDirBase$theFill --useFills $theFill
+# Do subset of triggers:
 python plotTriggerRates.py --triggerList=monitorlist_COLLISIONS.list --fitFile=Fits/2016/FOG.pkl --saveDirectory=$outputDirBase$theFill $theRuns
 
+# Do "All triggers" plots:
+python plotTriggerRates.py --triggerList=monitorlist_ALL.list --fitFile=Fits/AllTriggers/FOG.pkl --saveDirectory=$outputDirBase$theFill/AllTriggers $theRuns
+
+
+# This is just to grab the run numbers used in the fit:
 while read -r line
 do
     fitCommand="$line"
 done < "Fits/2016/command_line.txt"
 
+# Adjust the html content in the output dir:
 match='<html>'
-insertFirst='Runs used to produce fits:<br>'
+insertFirst='<h3>Runs used to produce fits:<br>'
 insertSecond=$(echo $fitCommand | sed 's/python plotTriggerRates.py --createFit --nonLinear --triggerList=monitorlist_COLLISIONS.list//')
+insertThird='</h3>'
+insertFourth='<h3><a href="./AllTriggers/">Larger Subset of Triggers</a></h3>'
+insertFifth='<h3>A Few Representative Triggers:</h3>'
 file=$outputDirBase$theFill/index.html
 
-sed -i "s/$match/$match\n$insertFirst\n$insertSecond/" $file
+sed -i "s#$match#$match\n$insertFirst\n$insertSecond$insertThird\n$insertFourth\n$insertFifth#" $file
