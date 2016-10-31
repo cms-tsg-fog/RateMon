78fa22c76da32bd503675f133d6b217249dfbc33
Andrew42 <awightma@nd.edu>
2016-10-31 17:31:50 +0100

    Reworked how L1 trigger rates are queried

--- PlotMaker.py
+++ PlotMaker.py
@@ -11,6 +11,7 @@
 #    { key:obj }             -- denotes a dictionary
 #    { key1: { key2:obj } }  -- denotes a nested dictionary
 #####################################################################
+
 import math
 import array
 import ROOT
