24be5f72a72b59f843fc68a7b1d7c1dc03ea0f24
Andrew42 <awightma@nd.edu>
2017-11-14 12:36:15 +0100

    Update mail alert sender info

--- mailAlert.py
+++ mailAlert.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 import DatabaseParser
 from datetime import datetime,timedelta
 import sys
+import socket
 sys.path.append('/nfshome0/hltpro/scripts')
 
 emailList = ["cms-tsg-fog@cern.ch", "cms-l1t-operations@cern.ch"]
@@ -56,32 +57,53 @@ def digest(hours,maxRate=35,printAll=False):
         text = text+" >> No Runs in last %d hours" % (hours,)
     return isBadRun,text
 
-def sendMail(email,subject,to,fro,msgtxt):
+def sendMail(email,subject,sender,msgtxt):
     msg = MIMEText(msgtxt)
     msg['Subject'] = subject
-    msg['From'] = fro
-    msg['To'] = to
+    msg['From'] = sender
+    msg['To'] = email
     s = smtplib.SMTP('localhost')
     #s.sendmail("hlt@cern.ch", email, msg.as_string())
-    s.sendmail(email, email, msg.as_string())
+    #s.sendmail(email, email, msg.as_string())
+    s.sendmail(sender,email,msg.as_string())
     s.quit()
 
 def mailAlert(text):
+    sender = "FOG <cms-tsg-fog@cern.ch>"
+    subject = "[HLTRateMon] Trigger Rate Warning"
+    host = "_".join(socket.gethostname().split('.'))
+    text += "\nSent from: %s" % host
     try:
         for email in emailList:
-            sendMail(email,"[HLTRateMon] Trigger Rate Warning", "HLT", "HLT", text)
+            #sendMail(email,"[HLTRateMon] Trigger Rate Warning","HLT",text)
+            sendMail(email,subject,sender,text)
             print "Mail sent to:", email
     except:
         print "Failed to send mail"
         print text
 
-
 if __name__=='__main__':
-    isBad,text = digest(1)
-    sendMail("a.zucchetta@cern.ch","[HLTRateMonDebug] Express Rate Digest","HLTDebug","HLTDebug",text)
-    sendMail("charles.mueller@cern.ch","[HLTRateMonDebug] Express Rate Digest","HLTDebug","HLTDebug",text)
-    try:
-        if isBad:
-            for email in emailList.emailList: sendMail(email,"[HLTRateMon] Express Rate Digest","HLT","HLT",text)
-    except:            
-        print text
+    sender = "FOG <cms-tsg-fog@cern.ch>"
+    rec = "andrew.steven.wightman@cern.ch"
+    text = "Some test text"
+    text += "\nSent from: %s" % socket.gethostname()
+
+    msg = MIMEText(text)
+    msg['Subject'] = "[HLTRateMonDebug] Test Subject"
+    msg['From'] = sender
+    msg['To'] = rec
+
+    #s = smtplib.SMTP('localhost')
+    #s.sendmail(sender,rec,msg.as_string())
+    #s.quit()
+
+    sendMail(rec,"[HLTRateMonDebug] Test Subject",sender,text)
+
+    #isBad,text = digest(1)
+    #sendMail("a.zucchetta@cern.ch","[HLTRateMonDebug] Express Rate Digest","HLTDebug","HLTDebug",text)
+    #sendMail("charles.mueller@cern.ch","[HLTRateMonDebug] Express Rate Digest","HLTDebug","HLTDebug",text)
+    #try:
+    #    if isBad:
+    #        for email in emailList.emailList: sendMail(email,"[HLTRateMon] Express Rate Digest","HLT","HLT",text)
+    #except:            
+    #    print text
