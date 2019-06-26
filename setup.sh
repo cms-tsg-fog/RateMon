9fdebdae2c6f2dbe762eb99d2510e6a63464bd57
Kelci Ann Mohrman <kmohrman@lxplus713.cern.ch>
2019-06-26 21:58:16 +0200

    Removing old/unused code

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-export SCRAM_ARCH=slc7_amd64_gcc630
-export VO_CMS_SW_DIR=/opt/offline
-source $VO_CMS_SW_DIR/cmsset_default.sh
-cd $VO_CMS_SW_DIR/$SCRAM_ARCH/cms/cmssw/CMSSW_9_2_10/src
-cmsenv
-cd -
