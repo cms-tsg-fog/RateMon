82e47c4b5d6c7cfe96506008a506de82c113fb51
Glenn Dirkx <glenn.dirkx@cern.ch>
2020-06-01 20:11:43 +0200

    typo

--- oms/start.sh
+++ oms/start.sh
@@ -4,4 +4,4 @@ IFS=$'\n\t\v'
 
 sed -i -- "s|CMS_WBM_R|${DB_USER}|g" /aggregator-config.yml
 sed -i -- "s|PASSWORD|${DB_PASS}|g" /aggregator-config.yml
-java -jar /aggregator.jar server /aggregator-config.yml
+java -jar /aggregator.jar server /aggregator.config.yml
