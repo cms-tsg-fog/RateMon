6e107333ead7c48966c6513e342f1b436bd8af03
Glenn Dirkx <glenn.dirkx@cern.ch>
2020-11-10 16:33:35 +0100

    log to a directory where hltpro has access

--- ratemon/Logger.py
+++ ratemon/Logger.py
@@ -6,9 +6,9 @@ class Logger(object):
         
         self.cdate = str(datetime.datetime.now()).split()[0]
         self.ctime = str(datetime.datetime.now()).split()[1].split(':')[0] +"_"+ str(datetime.datetime.now()).split()[1].split(':')[1]
+        self.logdir = os.getenv("RATEMON_LOG_DIR", default="/cmsnfshome0/nfshome0/triggershift/RateMonShiftTool/runII_2016/RateMon")
         self.fname = "log_rateMon_"+self.cdate+"_"+self.ctime+".dat"
-
-        self.log = open(self.fname, "a")
+        self.log = open(self.logdir + self.fname, "a")
 
     def write(self, message):
         self.terminal.write(message)
