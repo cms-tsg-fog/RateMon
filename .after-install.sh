d5d32bdaefdb7b18133fcab454985446870664d5
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2021-02-18 18:19:39 +0000

    Revert "Fix after-install path"
    This reverts commit 879a16cb862b2d9fec619647f3813205f1f40138

--- .after-install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-chmod a+w ratemon/dbConfig.yaml
-chmod a+w ratemon/.secure/
-source ratemon/.secure/.secureReplace.sh
---
+++ ratemon/.after-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+chmod a+w dbConfig.yaml
+chmod a+w .secure/
+source .secure/.secureReplace.sh
