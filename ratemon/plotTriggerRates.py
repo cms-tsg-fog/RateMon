24e1f010e6a70fe168e8fdeb55bba361346f1cca
Kelci Mohrman <kmohrman@nd.edu>
2021-07-13 22:27:10 +0200

    Pass true or false for useFills option

--- ratemon/plotTriggerRates.py
+++ ratemon/plotTriggerRates.py
@@ -32,6 +32,7 @@ class MonitorController:
             "triggerList="     : None,
             "saveDirectory="   : None,
             "useFit="          : None,
+            "useFills="        : None,
             "psFilter="        : None,
             "lsVeto="          : None,
             "rootFileName="    : None,
@@ -51,7 +52,6 @@ class MonitorController:
             #"vsInstLumi"      : None,
             "vsLS"             : None,
             #"useCrossSection" : None,
-            "useFills"         : None,
             "useBunches"       : None,
             "compareFits="     : None,
             "showFitRunGroups" : None,
@@ -158,6 +158,10 @@ class MonitorController:
                     self.rate_monitor.root_file_name = op_val
                 elif op_name == "saveDirectory=":
                    self.rate_monitor.save_dir = op_val
+                elif op_name == "useFills=":
+                    if op_val:
+                        self.rate_monitor.use_fills = True
+                        self.rate_monitor.plotter.color_by_fill = True  # Might want to make this an optional switch
                 elif op_name == "useFit=":
                     self.rate_monitor.plotter.default_fit = op_val
                     self.rate_monitor.make_fits  = True
@@ -284,9 +288,6 @@ class MonitorController:
                     self.rate_monitor.use_pileup = False
                     self.rate_monitor.use_lumi = False
                     self.rate_monitor.use_LS = True
-                elif op_name == "useFills":
-                    self.rate_monitor.use_fills = True
-                    self.rate_monitor.plotter.color_by_fill = True  # Might want to make this an optional switch
                 elif op_name == "useBunches":
                     self.rate_monitor.data_parser.normalize_bunches = False
                 elif op_name == "showFitRunGroups":
@@ -528,8 +529,8 @@ class MonitorController:
             #    self.rate_monitor.data_parser.use_cross_section = True
 
             elif label == "--useFills":
-                # Specify that the data should fetched by fill number
-                self.ops_dict["useFills"] = True
+                # Specify if the data should fetched by fill number
+                self.ops_dict["useFills="] = op
 
             elif label == "--useBunches":
                 # Don't try to normalize the rates by colliding bunches
@@ -717,7 +718,10 @@ class MonitorController:
         for k,v in kwargs.items():
             if k in self.ops_dict:
                 self.ops_dict[k] = v
+                print("in dict:",k)
             # FIXME: bad workaround
+            elif k == "useFills":
+                self.ops_dict["useFills="] = v
             elif k == "dbConfig":
                 self.ops_dict["dbConfigFile="] = v
             elif k == "triggerList":
@@ -727,8 +731,7 @@ class MonitorController:
             elif k == "data_lst":
                 self.usr_input_data_lst = v
             else:
-                print("\nError: Unknown option",k,", raising exception.")
-                raise Exception
+                raise Exception(f"\nError: Unknown option \"{k}\". Exiting...")
         # print (self.usr_input_data_lst, self.ops_dict)
         self.setOptions(self.ops_dict,self.usr_input_data_lst)
         return self.rate_monitor.run()
