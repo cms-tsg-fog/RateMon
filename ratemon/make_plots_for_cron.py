069e38baf790524b9a4f165a2aee3d8aee9bd0f0
Kelci Mohrman <kmohrman@lxplus759.cern.ch>
2021-11-01 15:20:18 +0100

    A few minor updates to how/where we save the output, plus use new parser

--- ratemon/make_plots_for_cron.py
+++ ratemon/make_plots_for_cron.py
@@ -2,7 +2,7 @@
 # For a full version of this script for Run 3, we will need the following three components:
 #    1) Get the latest runs from the DB
 #    2) Create the rate vs PU plots
-#    3) Move the plots and summary json to /cmsnfsrateplots/rateplots/ on kvm-s3562-1-ip151-84, or wherever we decide to put them
+#    3) Save and/or move the plots and summary json to /cmsnfsrateplots/rateplots/ on kvm-s3562-1-ip151-84 and/or to the dir on eos
 # How to run this script:
 #   - Run: "python3 create_plots_dump_trg_json.py"
 # Example of how to run this script with cron:
@@ -36,7 +36,7 @@ with open('/data/ratemon/ratemon/dbConfig.yaml', 'r') as stream:
 
 
 # Constructs the str for the out dir path
-def make_output_dir_str(base_dir,subdir_str,append_timestamp=True):
+def make_output_dir_str(base_dir,subdir_str,append_timestamp=False):
     sub_dir_name = subdir_str
     if append_timestamp:
         timestamp_tag = datetime.datetime.now().strftime('%Y%m%d_%H%M')
@@ -52,20 +52,20 @@ def main():
     parser = DBParser()
 
     # Get the runs from the latest fill with stable beams
-    #run_lst , fill_num = parser.getRecentRuns()
-    run_lst , fill_num = [324998,324999,325000,325001] , 7324 # Hard code specific runs, for testing
+    run_lst , fill_num = parser.getRecentRuns()
+    #run_lst , fill_num = [324998,324999,325000,325001] , 7324 # Hard code specific runs, for testing
+    print(f"Making plots for fill {fill_num}, runs: {run_lst}")
     if len(run_lst) == 0: raise Exception("Error: No runs specified. Exiting.")
-    else: print(f"Making plots for runs: {run_lst}")
 
     # Some placeholder options for where to save the plots
-    save_dir_base = os.getcwd() # For testing
+    #save_dir_base = os.getcwd() # For testing
     #save_dir_base = "/eos/user/k/kmohrman/www/rate_vs_PU_plots/checks_for_oms/" # For testing on lxplus (if you are kelci)
     save_dir_base = "/cmsnfsrateplots/rateplots/testing/" # For testing on kvm-s3562-1-ip151-84
     #save_dir_base = "/cmsnfsrateplots/rateplots/LS2/" # For LS2 tests on kvm-s3562-1-ip151-84
 
     # Can prepend "testing_ratemon" if we want
-    out_dir = make_output_dir_str(save_dir_base,"testing_ratemon_"+str(fill_num),append_timestamp=False)
-    #out_dir = make_output_dir_str(save_dir_base,str(fill_num))
+    #out_dir = make_output_dir_str(save_dir_base,"testing_ratemon_"+str(fill_num),append_timestamp=True)
+    out_dir = make_output_dir_str(save_dir_base,str(fill_num))
     print("Saving plots to:",out_dir)
 
     # Which triggers to use
@@ -73,7 +73,6 @@ def main():
 
     # Make the plots
     controller.runStandalone(
-        oldParser      = True,
         dbConfig       = dbCfg,
         saveDirectory  = out_dir,
         triggerList    = trigger_list,
