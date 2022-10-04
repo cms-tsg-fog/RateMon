0493c17e2552484d9556cc54b2a53735e29e24f7
Yuyi Wan <yuyi.wan@cern.ch>
2022-10-04 16:17:22 +0200

    add file for testing

--- ratemon/mattermostReport.py
+++ ratemon/mattermostReport.py
@@ -2,16 +2,16 @@ import requests
 import json
 import yaml
 
-def mattermostAlert(text):
-    try:
-        stream = open(str('mattermostHook.yaml'), 'r')
-        cfg  = yaml.safe_load(stream)
-        hook = cfg['mattermost_hook']
-    except:
-        print("Unable to get mattermost hook")
-        return
-    try:
-        url = f"https://mattermost.web.cern.ch/hooks/{hook}"
-        r = requests.post(url,data=json.dumps({"text" : text}))
-    except:
-        print("Unable to send message to mattermost")
+stream = open(str('mattermostHook.yaml'), 'r')
+cfg  = yaml.safe_load(stream)
+hook = cfg['mattermost_hook']
+
+text = 'sending mattermost alert'
+url = f"https://mattermost.web.cern.ch/hooks/{hook}"
+r = requests.post(url,data=json.dumps({"text" : text}))
+
+payload={"text": "Hello, this is some text\nThis is more text. :tada:"}
+#POST /hooks/{hook} HTTP/1.1
+#Host: marttermost.web.cern.ch
+#Content-Type: application/json
+#Content-Length: 63
