f80fb8ea9c3585de0148c281e9f734edd0ab6e8a
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2023-10-17 16:28:32 +0200

    Add script to automatically update all reference fits

--- ratemon/referenceRuns.py
+++ ratemon/referenceRuns.py
@@ -2,7 +2,7 @@
 
 referenceRuns = {
     'collisions' : [370580, 370772, 370774, 370775, 370776],
-    'collisionsHI': [374666, 374667, 374668],
-    'collisions900GeV': [353087],
-    'cosmics': [349527]
+    'collisionsHI' : [374666, 374667, 374668],
+    'collisions900GeV' : [353087],
+    'cosmics' : [349527]
 }
---
+++ ratemon/updateReferenceFits.py
@@ -0,0 +1,16 @@
+# script to update all reference fits, taking input from referenceRuns.py
+
+from plotTriggerRates import MonitorController 
+from referenceRuns import referenceRuns
+
+controller = MonitorController()
+
+controller.ops_dict["updateOnlineFits"] = True
+for run_type in referenceRuns.keys(): # ["collisions", "collisionsHI", "collisions900GeV", "cosmics"]
+    controller.usr_input_data_lst = referenceRuns[run_type] 
+    
+    controller.ops_dict["triggerList="] = "TriggerLists/monitorlist_%s.list"%run_type.upper()
+    controller.run()
+
+    controller.ops_dict["allTriggers"] = True
+    controller.run()
